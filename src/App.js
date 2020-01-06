d2093b093d42dee038981fb5c27df3a31fac90c6
jean carlos <jeanc.aldea1@gmail.com>
2020-01-06 13:39:52 -0300

    commit2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
 import './styles/styles.scss';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    // code copiado de edgird
+    // RECORDAR QUE CLASS DEBE SER CLASSNAME PORQUE ES JSX
+    <div className="main-banner img-container" id="main-banner">
+      <div className="ed-grid lg-grid-6">
+        <div className="lg-cols-4 lg-x-2">
+          <img className="main-banner__img" alt="banner" src="https://www.redeszone.net/app/uploads/2018/11/tsurugi-distribucion-linux.jpg"/>
+          <div className="main-banner__data s-center">
+            <p className="t2 s-mb-0">Este es mi banner</p>
+            <p> Y este es el Subtítulo de mi  banner</p>
+            <a href="www.google.com" className="button">Botón del banner</a>
+          </div>
+        </div>
     </div>
+</div>
   );
 }
 
