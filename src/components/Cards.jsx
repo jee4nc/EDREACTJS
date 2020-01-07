2acfef837d75a99c0c2a0121188419e4ef7b3107
jean carlos <jeanc.aldea1@gmail.com>
2020-01-07 02:16:14 -0300

    Manejando_props En este commit exporto props para darle a cada cards su image

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,30 +1,44 @@
 import React, {Component} from 'react';
 
-class Cards extends Component {
-    render() {
-        return <article className="card">
+// CONSTANTE EN LA CUAL SE INGRESA NOMBRE Y OTRO ATRIBUTOS
+const persona = {"nombre": "Jean", "apellido": "Aldea" , "edad": 21}
+// CONSTANTE EN LA CUAL SE CREA UN METODO QUE MARQUE LA MAYORIA DE EDAD COMO 18
+const mayoredad = edad => edad > 18
+
+// ACA DIGO QUE RECIBA EL OBJETO props
+// Posteriormente, cambio props para poner los atributos del objeto props
+const Cards = ({title,image,price}) => (
+    <article className="card">
         <div className="img-container s-ratio-16-9 s-radius-tr s-radius-tl">
-            <img src="http://www.milapuntocom.com/wp-content/uploads/2015/08/cats-portada-480x480.jpg" alt="cardss"/>
+            {/* ACA ESTOY PONIENDO ATRIBUTOS DINAMICOS DENTRO DE JSX */}
+            <img src={image} alt={title}/>
         </div>
         <div className="card__data s-border s-radius-br s-radius-bl s-pxy-2">
             <h3 className="t5 s-mb-2 s-center">
-            Programación orientada a objetos con Go
+            {`Hola me llamo ${persona.nombre} y tengo ${persona.edad} años`}
             </h3>
             <div className="s-mb-2 s-main-center">
             <div className="card__teacher s-cross-center">
                 <div className="card__avatar s-mr-1">
                 <div className="circle img-container">
-                    <img src="" alt="cardss"/>
+                    <img src={image} alt="cardss"/>
                 </div>
                 </div>
-                <span className="small">Alexys Lozada</span>
+            <span className="small">
+                {
+                    // ACA SE OCUPA LA EXPRESION PARA CREAR UN CICLO QUE DEVUELVA SEGUN LA EDAD
+                    mayoredad(persona.edad)
+                    ?  <h3>Soy mayor de edad</h3> 
+                    :  <p>Soy menor de edad</p>
+                }
+            </span>
             </div>
             </div>
             <div className="s-main-center">
-            <a className="button--ghost-alert button--tiny" href="www.google.cl">$ 20USD</a>
+    <a className="button--ghost-alert button--tiny" href="www.google.cl">{price}</a>
             </div>
         </div>
     </article>
-    }
-}
+)
+
 export default Cards;
