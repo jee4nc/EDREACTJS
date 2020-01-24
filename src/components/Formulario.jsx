603eb99658f18e92ccc9884d036dda5276da2969
Jean-Carlos <jeanc.aldea1@gmail.com>
2020-01-23 21:10:43 -0300

    Save

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -4,7 +4,13 @@ class Formulario extends Component{
     // METODO CONSTRUCTOR QUE RECIBE LAS PROPS Y LAS ASIGNA A COMPONENT MEDIANTE SUPER
     constructor(props) {
         super(props)
+        // EL state me sirve para poder obtener el valor de un objeto el cual se va actualizando
+        this.state = {
+            nombre: "",
+            correo: ""
+        }
     }
+
     render() {
         return (
             <div className="ed-grid">
@@ -12,16 +18,26 @@ class Formulario extends Component{
                 <form>
                     <div className="form__item">
                         <label>Nombre Completo</label>
-                        <input type="text"/>
+                        <input
+                            type="text" 
+                            onChange={ (dato) => this.setState({
+                                nombre: dato.target.value
+                            })}/>
                     </div>
                     <div className="form__item">
                         <label>Correo Electronico</label>
-                        <input type="email"/>
+                        <input type="email" onChange={ (dato) => this.setState({
+                            correo: dato.target.value
+                        })} />
                     </div>
                     <div className="form__item">
                         <input className="button full" type="submit" value="Enviar"/>
                     </div>
                 </form>
+                <div>
+                    <h2>{`Hola ${this.state.nombre}`}</h2>
+                    <span>{`Tu correo es ${this.state.correo}`}</span>
+                </div>
             </div>
         )
     }
